85f967e7ec9f5fdcbb14fd14c14d472334039e24
Sonny812 <sonny@milton.pro>
2020-06-12 01:48:57 +0300

    Add extra task

---
+++ extra_task/extra_task/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace extra_task
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtbxRadius = new System.Windows.Forms.TextBox();
+			this.cmbbxRadius = new System.Windows.Forms.ComboBox();
+			this.txtbxLog = new System.Windows.Forms.TextBox();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.btnDisplayArea = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(23, 32);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(43, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Радиус";
+			// 
+			// txtbxRadius
+			// 
+			this.txtbxRadius.Location = new System.Drawing.Point(75, 29);
+			this.txtbxRadius.Name = "txtbxRadius";
+			this.txtbxRadius.Size = new System.Drawing.Size(137, 20);
+			this.txtbxRadius.TabIndex = 1;
+			// 
+			// cmbbxRadius
+			// 
+			this.cmbbxRadius.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbbxRadius.FormattingEnabled = true;
+			this.cmbbxRadius.Location = new System.Drawing.Point(228, 28);
+			this.cmbbxRadius.Name = "cmbbxRadius";
+			this.cmbbxRadius.Size = new System.Drawing.Size(121, 21);
+			this.cmbbxRadius.TabIndex = 2;
+			// 
+			// txtbxLog
+			// 
+			this.txtbxLog.Location = new System.Drawing.Point(396, 17);
+			this.txtbxLog.Multiline = true;
+			this.txtbxLog.Name = "txtbxLog";
+			this.txtbxLog.ReadOnly = true;
+			this.txtbxLog.Size = new System.Drawing.Size(561, 209);
+			this.txtbxLog.TabIndex = 3;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(26, 203);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(75, 23);
+			this.btnAdd.TabIndex = 4;
+			this.btnAdd.Text = "Добавить";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+			// 
+			// btnDisplayArea
+			// 
+			this.btnDisplayArea.Location = new System.Drawing.Point(121, 203);
+			this.btnDisplayArea.Name = "btnDisplayArea";
+			this.btnDisplayArea.Size = new System.Drawing.Size(75, 23);
+			this.btnDisplayArea.TabIndex = 4;
+			this.btnDisplayArea.Text = "Площадь";
+			this.btnDisplayArea.UseVisualStyleBackColor = true;
+			this.btnDisplayArea.Visible = false;
+			this.btnDisplayArea.Click += new System.EventHandler(this.btnDisplayArea_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(981, 257);
+			this.Controls.Add(this.btnDisplayArea);
+			this.Controls.Add(this.btnAdd);
+			this.Controls.Add(this.txtbxLog);
+			this.Controls.Add(this.cmbbxRadius);
+			this.Controls.Add(this.txtbxRadius);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Дополнительное задание";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtbxRadius;
+		private System.Windows.Forms.ComboBox cmbbxRadius;
+		private System.Windows.Forms.TextBox txtbxLog;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnDisplayArea;
+	}
+}
+
---
+++ extra_task/extra_task/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace extra_task
+{
+	public partial class Form1 : Form
+	{
+		class Unit
+		{
+			public readonly string Name;
+			public readonly double Coefficient;
+
+			public Unit(string name, double coefficient)
+			{
+				this.Name = name;
+				this.Coefficient = coefficient;
+			}
+
+			public override string ToString()
+			{
+				return this.Name;
+			}
+		}
+
+		class Measure
+		{
+			public readonly double Value;
+			public readonly Unit Unit;
+
+			public Measure(double value, Unit unit)
+			{
+				this.Value = value;
+				this.Unit = unit;
+			}
+
+			public double ToDefaultUnit()
+			{
+				return this.Unit.Coefficient * this.Value;
+			}
+
+			public override string ToString()
+			{
+				return $"{this.Value} {this.Unit}";
+			}
+		}
+
+		class Circle
+		{
+			public readonly Measure Radius;
+			public Measure Diameter { get => new Measure(this.Radius.Value * 2, this.Radius.Unit); }
+
+			public Circle(Measure radius)
+			{
+				this.Radius = radius;
+			}
+
+			public virtual double Area()
+			{
+				return Math.PI * this.Radius.ToDefaultUnit() * this.Radius.ToDefaultUnit(); 
+			}
+		}
+
+		class Sphere : Circle
+		{
+			public Sphere(Measure radius) : base(radius)
+			{ 
+			}
+
+			public override double Area()
+			{
+				var r = this.Radius.ToDefaultUnit();
+
+				return 4 * Math.PI * r * r;
+			}
+
+			public double Volume()
+			{
+				return (4.0 / 3.0) * Math.PI * Math.Pow(this.Radius.ToDefaultUnit(), 3);
+			}
+		}
+		
+		Unit[] units = new Unit[] {
+			new Unit("Милиметр", 0.001),
+			new Unit("Сантиметр", 0.01),
+			new Unit("Дециметр", 0.1),
+			new Unit("Метр", 1.0),
+			new Unit("Киломтер", 1000),
+		};
+
+		List<Sphere> spheres = new List<Sphere>();
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			this.cmbbxRadius.Items.AddRange(units);
+			this.cmbbxRadius.SelectedIndex = 3;
+		}
+
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+			if (!this.ValidateUserInput())
+			{
+				return;
+			}
+
+			var sphere = new Sphere(new Measure(double.Parse(this.txtbxRadius.Text), this.cmbbxRadius.SelectedItem as Unit));
+
+			this.spheres.Add(sphere);
+
+			this.txtbxLog.Text += $"Добавлена новая сфера. Радиус: {sphere.Radius}, площадь поверхности: {sphere.Area()} метр, объем: {sphere.Volume()} метр" + Environment.NewLine;
+
+			if (this.spheres.Count == 5)
+			{
+				this.btnAdd.Enabled = false;
+				this.btnDisplayArea.Visible = true;
+			}
+		}
+
+		private bool ValidateUserInput()
+		{
+			if (!double.TryParse(txtbxRadius.Text, out double radius) || radius <= 0)
+			{
+				MessageBox.Show("Данные введены некорректно. Радиус должен быть числом и больше 0");
+
+				return false;
+			}
+
+			return true;
+		}
+
+		private void btnDisplayArea_Click(object sender, EventArgs e)
+		{
+			this.txtbxLog.Text += $"Сумма площадей всех созданных сфер: {this.spheres.Sum(x => x.Area())} м";
+		}
+	}
+}
